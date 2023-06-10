8405f94a43836b67f8c4ba386e18643ecdbb2765
Luciano <lucianoneto1033@gmail.com>
2023-06-10 00:40:56 -0300

    feat: trava temporizada até dia 11/06

--- App.tsx
+++ App.tsx
@@ -1,10 +1,14 @@
 import React from "react";
-import { SafeAreaView } from 'react-native';
+import { SafeAreaView, View, Image } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
 import GlobalStyles from './assets/styles/GlobalStyles';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Toast from 'react-native-toast-message';
+import moment from 'moment';
+import endPrototype from "./assets/images/endprototype.png";
+const currentDate = moment();
+const lockedDate = moment('2023-06-11');
 
 import AppTabView from './src/screens/AppTabView';
 import Emergency from './src/screens/AppTabs/Emergency';
@@ -15,22 +19,29 @@ const Stack = createNativeStackNavigator();
 
 const App = () => {
 
-  return (
-    <>
-      <SafeAreaView style={GlobalStyles.SafeArea}>
-        <StatusBar style="light" networkActivityIndicatorVisible animated translucent backgroundColor={Colors.primary} />
-        <NavigationContainer>
-          <Stack.Navigator initialRouteName='AppTabView'>
-            <Stack.Screen options={{ headerShown: false }} name="AppTabView" component={AppTabView} />
-            <Stack.Screen options={{ headerShown: false }} name="Emergency" component={Emergency} />
-            <Stack.Screen options={{ headerShown: false }} name="Report" component={Report} />
-            <Stack.Screen options={{ headerShown: false }} name="Protocols" component={Protocols} />
-          </Stack.Navigator>
-        </NavigationContainer>
-      </SafeAreaView>
-      <Toast />
-    </>
-  );
+  if (currentDate.isBefore(lockedDate)) {
+    return (
+      <>
+        <SafeAreaView style={GlobalStyles.SafeArea}>
+          <StatusBar style="light" networkActivityIndicatorVisible animated translucent backgroundColor={Colors.primary} />
+          <NavigationContainer>
+            <Stack.Navigator initialRouteName='AppTabView'>
+              <Stack.Screen options={{ headerShown: false }} name="AppTabView" component={AppTabView} />
+              <Stack.Screen options={{ headerShown: false }} name="Emergency" component={Emergency} />
+              <Stack.Screen options={{ headerShown: false }} name="Report" component={Report} />
+              <Stack.Screen options={{ headerShown: false }} name="Protocols" component={Protocols} />
+            </Stack.Navigator>
+          </NavigationContainer>
+        </SafeAreaView>
+        <Toast />
+      </>
+    );
+  }
+  else {
+    return (
+      <Image style={{ width: "100%", height: "100%" }} source={endPrototype} resizeMode="contain" />
+    )
+  }
 }
 
 export default App;
